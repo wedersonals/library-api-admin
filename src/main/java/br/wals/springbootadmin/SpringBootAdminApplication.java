230014c7eb8b294a339bf89ab13adbc76adc7f5d
wedersonals <wederson_adriano@hotmail.com>
2022-08-15 19:50:44 -0300

    Implementado servidor de administração do projeto Library API com Spring Boot Admin

---
+++ src/main/java/br/wals/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,17 @@
+package br.wals.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
